40b43deb72e2adf95b2bf998206ef1d403c10773
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-13 20:46:49 -0800

    src/api/moviedb.ts getPopularMovies() Resolves #13

--- frontend/src/api/moviedb.ts
+++ frontend/src/api/moviedb.ts
@@ -3,4 +3,11 @@ import { env } from "~/env";
 export class MovieDBAPI {
   static baseUrl = "https://api.themoviedb.org/3";
   static apiKey = env.MOVIEDB_API_KEY;
+
+  static getPopularMovies = async (page = 1) => {
+    const response = await fetch(
+      `${MovieDBAPI.baseUrl}/movie/popular?api_key=${MovieDBAPI.apiKey}&page=${page}`,
+    );
+    return response.json();
+  };
 }
