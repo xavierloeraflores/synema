1912f6bc81a196e2eb860fe0a8e736bd52bebe77
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-13 20:07:07 -0800

    src/pages/_app.tsx: Implemented layout

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import Layout from "~/components/layout";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 };
 
 export default MyApp;
